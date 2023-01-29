2461e54c2eb64b25f2dd52894d9ba0810cedbf6f
Kavindu-Rankothge <kavindur@gmail.com>
2023-01-29 18:32:57 +1100

    Upadate post status after creating video

--- MainSetup.py
+++ MainSetup.py
@@ -9,9 +9,23 @@ from RedditScrapper import update_data
 
 # returns top non-used post details from dataframe
 def get_top_post(df):
-    df = df.sort_values('score',ascending = False)
-    temp_df = df[df['posted'] == False] 
+    df = df.sort_values('score', ascending = False)
+    temp_df = df[df['posted'] == False]
     return temp_df.iloc[0]
+
+# changes post status after posted
+def update_posted_status(df, id):
+    idx = df.index[df['id'] == id].tolist()
+    df.at[idx[0],'posted'] = True
+    return df
+
+# function posts video on tiktok
+def post_video(title):
+    prompt = input('Do you want to post video on tiktok? ')
+    if (prompt == 'y' or prompt == 'yes'):
+        # TODO post_tiktok()
+        print('Post it yourself smh')
+        pass
 
 # main method handles all scripts
 if __name__ == '__main__':
@@ -29,4 +43,11 @@ if __name__ == '__main__':
     save_comment_audios(post['title']+' '+post['selftext'], post['id'], post['comments'])
     print('\nEditing Video...')
     generate_video(post['id'])
+    # save title of file
+    with open('assets/'+post['id']+'/details.txt', 'w') as f:
+        f.write(post['title'])
+    print('\nFile can be found in: assets/'+post['id']+' folder.')
+    post_video(post['title'])
+    df = update_posted_status(df, post['id'])
+    df.to_json('assets/reddit-data.json')
     print('Done!')
